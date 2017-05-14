f8539982f42108a93cd1fe51c8a1eeb28a321c02
mistkafka <mistkafka@gmail.com>
2017-05-15 01:49:07 +0800

    将org-mode配置迁移到mistkafka-org layer中

---
+++ layers/mistkafka-org/packages.el
@@ -0,0 +1,47 @@
+(defconst mistkafka-org-packages
+  '(
+    org
+    )
+  )
+
+(defun mistkafka-org/post-init-org ()
+  (setq org-todo-keywords
+        (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d@/!)")
+                (sequence "PROJECT(p)" "|" "DONE(d@/!)" "CANCELLED(c@/!)")
+                (sequence "WAITING(w@/!)" "DELEGATED(e!)" "HOLD(h)" "|" "CANCELLED(c@/!)")))
+        org-todo-repeat-to-state "NEXT")
+
+  (setq org-todo-keyword-faces
+        (quote (("NEXT" :inherit warning)
+                ("PROJECT" :inherit font-lock-string-face))))
+
+  ;; config logbook
+  ;; Save clock data and notes in the LOGBOOK drawer
+  (setq org-clock-into-drawer t)
+  ;; Save state changes in the LOGBOOK drawer
+  (setq org-log-into-drawer t)
+  ;; Removes clocked tasks with 0:00 duration
+  (setq org-clock-out-remove-zero-time-clocks t)
+
+  ;; Show clock sums as hours and minutes, not "n days" etc.
+  (setq org-time-clocksum-format
+        '(:hours "%d" :require-hours t :minutes ":%02d" :require-minutes t))
+
+  ;; org-publish-project
+  (require 'ox-publish)
+  (setq org-publish-project-alist
+        '(
+          ("gitpage"
+           :base-directory "~/mistkafka.github.io/org/"
+           :base-extension "org"
+           :publishing-directory "~/mistkafka.github.io/_posts/"
+           :recursive t
+           :publishing-function org-html-publish-to-html
+           :html-extension "html"
+
+           :body-only t
+           :with-toc nil
+           :section-numbers nil
+           )
+          ))
+  )
---
+++ layers/mistkafka/layers.el
@@ -0,0 +1,3 @@
+(configuration-layer/declare-layers '(
+                                      mistkafka-org
+                                      ))
