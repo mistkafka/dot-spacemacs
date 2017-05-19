1f71ef0fba8250625c107f810cd5fadac3d4971c
mistkafka <mistkafka@gmail.com>
2017-05-20 01:11:57 +0800

    :wheelchair: 使用mistkafka-js/toggle-indent-level来替代mistkafka-js/set-indent-level
    mistkafka-js/set-indent-level需要手动输入缩进，我们一般输入2或者4，但是也可能手误输入5 6 7.

--- layers/mistkafka-js/funcs.el
+++ layers/mistkafka-js/funcs.el
@@ -1,4 +1,11 @@
+(defun mistkafka-js/toggle-indent-level ()
+  "Toggle js indent in 4 or 2"
+  (interactive)
+  (if (= js-indent-level 2)
+      (mistkafka-js/set-indent-level 4)
+    (mistkafka-js/set-indent-level 2)))
+
 (defun mistkafka-js/set-indent-level (indent)
-  (interactive "n")
   (setq-default js-indent-level indent)
-  (setq-default js2-basic-offset indent))
+  (setq-default js2-basic-offset indent)
+  (message "js indent is set to %d" indent))
