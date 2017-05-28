d0cf4857eb164dd7d51a3ab031f8297d1f64f174
mistkafka <mistkafka@gmail.com>
2017-05-29 00:30:36 +0800

    :sparkles: apple-open-current-file

--- layers/mistkafka/funcs.el
+++ layers/mistkafka/funcs.el
@@ -1,9 +1,13 @@
-(defun mistkafka/copy-file-name-to-clipboard ()
-  "Copy the current buffer file name to the clipboard."
-  (interactive)
+(defun mistkafka/private-get-file-name ()
   (let ((filename (if (equal major-mode 'dired-mode)
                       default-directory
                     (buffer-file-name))))
+    filename))
+
+(defun mistkafka/copy-file-name-to-clipboard ()
+  "Copy the current buffer file name to the clipboard."
+  (interactive)
+  (let ((filename (mistkafka/private-get-file-name)))
     (when filename
       (kill-new filename)
       (message "Copied buffer filename '%s' to the clipboard." filename))))
@@ -11,3 +15,12 @@
 (defun mistkafka/insert-currrent-date ()
   (interactive)
   (insert (shell-command-to-string "echo -n $(date +%Y-%m-%d)")))
+
+(defun mistkafka/apple-open-current-file ()
+  "Use apple's command 'open' to open current file"
+  (interactive)
+  (let* ((filename (mistkafka/private-get-file-name))
+         (cmd (format "open %s" filename)))
+    (and filename
+         (shell-command cmd))))
+
