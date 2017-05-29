93f46bd39132ad93352fe20b5601f60b0ec67582
mistkafka <mistkafka@gmail.com>
2017-05-29 16:08:43 +0800

    :sparkles: wrapper-region-with
    我们经常需要选中一段文本，用一对括号或者引号等字符将其包裹起来。现在又了wrapper-region-with，这个操作将会方便很多。

--- layers/mistkafka/funcs.el
+++ layers/mistkafka/funcs.el
@@ -24,3 +24,18 @@
     (and filename
          (shell-command cmd))))
 
+;; TODO: 这个方法有如下几个待改进的点：
+;;       1. 改成成只需要输入一个字符，如：输入'('或者')'即可知道是要用'()'包裹字符串
+;;       2. 开启vim模式。在vim的正常模式下，选中一段文本，然后按下某个字符就会包裹字符串
+(defun mistkafka/wrapper-region-with (str)
+  "Wrapper region string with a pair char. the str is like '()' or '[]'."
+  (interactive "sWrapper with:")
+  (if (use-region-p)
+      (let* ((input-str (buffer-substring-no-properties (region-beginning) (region-end)))
+             (output-str (format "%s%s%s" (substring str 0 1) input-str (substring str 1 2)))
+             (from (region-beginning))
+             (to (region-end)))
+        (save-excursion
+          (delete-region from to)
+          (goto-char from)
+          (insert output-str)))))
